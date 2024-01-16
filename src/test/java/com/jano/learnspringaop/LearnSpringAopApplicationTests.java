10dab6a44a46e6055585f3719453b6464036aa62
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-16 11:54:49 +0100

    init repo

---
+++ src/test/java/com/jano/learnspringaop/LearnSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jano.learnspringaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
