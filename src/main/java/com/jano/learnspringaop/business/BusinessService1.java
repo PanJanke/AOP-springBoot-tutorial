ac1499d855fadcfbd123dd3dc850eaa7d819eaf4
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-16 12:05:20 +0100

    Setting up Spring Components for Spring AOP

---
+++ src/main/java/com/jano/learnspringaop/business/BusinessService1.java
@@ -0,0 +1,23 @@
+package com.jano.learnspringaop.business;
+
+import java.util.Arrays;
+
+import com.jano.learnspringaop.data.DataService1;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class BusinessService1 {
+
+    private DataService1 dataService1;
+
+    public BusinessService1(DataService1 dataService1) {
+        this.dataService1 = dataService1;
+    }
+
+    public int calculateMax() {
+        int[] data = dataService1.retrieveData();
+        return Arrays.stream(data).max().orElse(0);
+    }
+}
