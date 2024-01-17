982f435b3ea3c40a2d8a4f60418d62abb77a4749
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-17 09:17:41 +0100

    @AfterReturning, @AfterThrowing

--- src/main/java/com/jano/learnspringaop/aspects/LoggingAspect.java
+++ src/main/java/com/jano/learnspringaop/aspects/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.jano.learnspringaop.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +12,37 @@ public class LoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-
-    @Before("execution(* com.jano.learnspringaop.business.*.*(..))")//WHEN
-    public void logMethodCall(JoinPoint joinPoint) {
+    //Pointcut - When?
+    // execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.jano.learnspringaop.*.*.*(..))")
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint) {
         logger.info("Before Aspect - {} is called with arguments: {}"
                 ,  joinPoint, joinPoint.getArgs());//WHAT
     }
+
+    @After("execution(* com.jano.learnspringaop.*.*.*(..))")
+    public void logMethodCallAfterExecution(JoinPoint joinPoint) {
+        logger.info("After Aspect - {} has executed",  joinPoint);
+    }
+
+    @AfterThrowing(
+            pointcut = "execution(* com.jano.learnspringaop.*.*.*(..))",
+            throwing = "exception"
+    )
+    public void logMethodCallAfterException(JoinPoint joinPoint, Exception exception) {
+        logger.info("AfterThrowing Aspect - {} has thrown an exception {}"
+                ,  joinPoint, exception);
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.jano.learnspringaop.*.*.*(..))",
+            returning = "resultValue"
+    )
+    public void logMethodCallAfterSuccessfulExecution(JoinPoint joinPoint,
+                                                      Object resultValue) {
+        logger.info("AfterReturning Aspect - {} has returned {}"
+                ,  joinPoint, resultValue);
+    }
+
 
 }
